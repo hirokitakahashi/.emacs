950870c2db145d8147e9a53e52fc6a1fab61e41f
unknown <hiroki@.(none)>
2011-01-31 10:46:27 +0000

    yasnippet of python mode modified.

--- my.el
+++ my.el
@@ -113,7 +113,8 @@
 ;=======================================================================
 
 (require 'yasnippet-bundle)
-(yas/define-snippets 'python-mode '(("env" "#!/usr/bin/env python" "#!/usr...")))
+(yas/define-snippets 'python-mode '(("env" "#!/usr/bin/env python
+# -*- coding: iso-8859-15 -*-" "#!/usr...")))
 
 (yas/define-snippets 'html-mode '(("doctype" "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">" "<!DOCTYPE ...")))
 
